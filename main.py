989da0e58c748442e3b823bd1fce04f7b039ab76
Stephen Hanson <spaehanson1@gmail.com>
2025-09-29 09:11:45 -0500

    add function call tools to request

--- main.py
+++ main.py
@@ -4,13 +4,29 @@ from enum import Enum
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import get_files_info, schema_get_files_info
 
 # Load environment variables from .env file
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 
 def main():
+    system_prompt = """
+                    You are a helpful AI coding agent.
+
+                    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+                    - List files and directories
+
+                    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+                    """
     # grab args from command line
     user_args = handle_args()
     if len(sys.argv) >= 1:
@@ -19,9 +35,17 @@ def main():
         client = genai.Client(api_key=api_key)
         response = client.models.generate_content(
             model='gemini-2.0-flash-001',
-            contents=messages
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            )
         )
         print(response.text)
+        if response.function_calls:
+            for function_call_part in response.function_calls:
+                print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        else:
+            print(response.text)
         if user_args.get('verbose', False):
             print(f"User prompt: {user_args['prompt']}")
             print(
